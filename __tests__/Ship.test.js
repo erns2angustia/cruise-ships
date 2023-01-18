3f68ade645e095719b7c55559c77a56b61a93c40
Ernesto Angustia <ernesto.angustia@gmail.com>
2023-01-18 17:19:01 +0000

    Building Cruise Ships

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -4,18 +4,30 @@ const Port = require('../src/Port.js');
 const Itinerary = require('../src/Itinerary.js');
 
 describe('Ship', () => {
+    describe('with ports and an itinerary', () => {
+        let ship;
+        let dover;
+        let calais;
+        let itinerary;
+
+        beforeEach(() => {
+        const dover = new Port('Dover');
+        const calais = new Port('Calais');
+        const itinerary = new Itinerary([dover, calais]);
+        const ship = new Ship(itinerary);
+        });
     it('can be instatiated', () => {
-        const port = new Port('Dover');
-        const itinerary = new Itinerary([port]);
+        const dover = new Port('Dover');
+        const itinerary = new Itinerary([dover]);
         const ship = new Ship(itinerary);
         expect(ship).toBeInstanceOf(Object);
     });
     });
     it('has a starting port', () => {
-        const port = new Port('Dover');
-        const itinerary = new Itinerary([port]);
+        const dover = new Port('Dover');
+        const itinerary = new Itinerary([dover]);
         const ship = new Ship (itinerary);
-        expect(ship.currentPort).toBe(port);
+        expect(ship.currentPort).toBe(dover);
     });
     it('can set sail', () => {
         const dover = new Port('Dover');
@@ -51,3 +63,4 @@ describe('Ship', () => {
         const ship = new Ship(itinerary);
         expect(dover.ships).toContain(ship);
       });
+    });
